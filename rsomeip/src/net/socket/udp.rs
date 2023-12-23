262305b7ced761ca3e335e68883c4cead4456e19
brunoldsilva <brunoldsilva@pm.me>
2023-12-23 11:54:55 +0000

    Fix UDP socket packet processing.
    These changes fix an issue with the message processing where the cancellation of  `recv()` would cause the buffer to be dropped.
    
    Now, `recv()` runs on its own task independently from the message processing loop.

--- rsomeip/src/net/socket/udp.rs
+++ rsomeip/src/net/socket/udp.rs
@@ -15,9 +15,8 @@ mod tests;
 /// An abstraction over an UDP socket.
 #[derive(Debug)]
 pub struct Socket {
-    inner: tokio::net::UdpSocket,
+    inner: Arc<tokio::net::UdpSocket>,
     packets: mpsc::Sender<Packet>,
-    buffers: BufferPool,
 }
 
 impl Socket {
@@ -27,38 +26,25 @@ impl Socket {
     ///
     /// Returns an error if the UDP socket cannot bind to the given `address`.
     pub async fn bind(address: SocketAddr) -> Result<(Self, mpsc::Receiver<Packet>)> {
-        let inner = UdpSocket::bind(address).await?;
+        let inner = UdpSocket::bind(address).await.map(Arc::new)?;
         let (packets, receiver) = mpsc::channel(32);
-        Ok((
-            Self {
-                inner,
-                packets,
-                buffers: BufferPool::new(),
-            },
-            receiver,
-        ))
+        Ok((Self { inner, packets }, receiver))
     }
 
     /// Processes messages until the receiver is closed or the socket becomes unavailable.
     pub async fn process(&mut self, mut messages: mpsc::Receiver<Message>) {
-        loop {
-            tokio::select! {
-                message = messages.recv() => {
-                    match message {
-                        Some(Message { operation, response }) => {
-                            self.process_operation(operation, response).await;
-                            continue;
-                        },
-                        None => break,
-                    }
-                },
-                received = self.recv() => {
-                    if received.is_err() {
-                        break;
-                    }
-                },
-            }
+        let (socket, packets) = (self.inner.clone(), self.packets.clone());
+        let receiver_handle = tokio::spawn(async move {
+            recv(socket, packets).await;
+        });
+        while let Some(Message {
+            operation,
+            response,
+        }) = messages.recv().await
+        {
+            self.process_operation(operation, response).await;
         }
+        receiver_handle.abort();
     }
 
     /// Performs the provided `operation` and sends the result to the `response` channel.
@@ -140,29 +126,29 @@ impl Socket {
         }
         Ok(())
     }
+}
 
-    /// Receives a packet from the socket and sends it to the `packets` channel.
-    ///
-    /// Uses a buffer from the `buffers` pool to store the received data.
-    ///
-    /// # Errors
-    ///
-    /// Returns an error if the socket was unable to receive.
-    async fn recv(&mut self) -> Result<()> {
-        let mut buffer = self.buffers.pull_or_else(|| Arc::new([0u8; 1024]));
+/// Receives packets from the socket and sends them to the `packets` channel.
+///
+/// It will keep receiving packets until the socket becomes unavailable, or the `packets` channel
+/// is closed.
+async fn recv(socket: Arc<UdpSocket>, packets: mpsc::Sender<Packet>) {
+    let mut buffers = BufferPool::new();
+    loop {
+        let mut buffer = buffers.pull_or_else(|| Arc::new([0u8; 1024]));
         let Some(mut_buffer) = Arc::get_mut(&mut buffer) else {
             // This should never happen. `pull_or_else()` will always return a unique buffer.
-            return Err(Error::Failure("buffer is not unique"));
+            eprintln!("buffer is not unique");
+            break;
         };
-        let result = match self.inner.recv_from(mut_buffer).await {
-            Ok((_, address)) => self
-                .packets
-                .send((address, buffer.clone()))
-                .await
-                .map_err(|_| Error::Failure("sender is closed")),
-            Err(err) => Err(Error::from(err)),
+        let Ok((_size, address)) = socket.recv_from(mut_buffer).await else {
+            println!("socket is unavailable");
+            break;
         };
-        self.buffers.push(buffer);
-        result
+        let Ok(()) = packets.send((address, buffer.clone())).await else {
+            println!("packets channel is closed");
+            break;
+        };
+        buffers.push(buffer);
     }
 }
--- rsomeip/src/net/socket/udp/tests.rs
+++ rsomeip/src/net/socket/udp/tests.rs
@@ -1,6 +1,13 @@
 use super::*;
 use crate::testing::ipv4;
 
+/// Checks that the socket can bind to an address.
+///
+/// Steps:
+/// 1. Create a [`Socket`] and bind to an address.
+///
+/// Checks:
+/// - The socket should bind to the address.
 #[tokio::test]
 async fn bind() {
     let _socket = Socket::bind(ipv4!([127, 0, 0, 1]))
@@ -8,21 +15,85 @@ async fn bind() {
         .expect("should bind to the address");
 }
 
+/// Checks that the socket can send data.
+///
+/// Steps:
+/// 1. Create a [`Socket`] and start processing messages.
+/// 2. Create a native socket to receive the data.
+/// 3. Send the data from the [`Socket`].
+/// 4. Receive the data on the native socket.
+///
+/// Checks:
+/// - The socket should send the data.
+/// - The socket should send the data to the correct address.
+/// - The socket should send the correct data.
 #[tokio::test]
 async fn send() {
+    // Create a new [`Socket`] and start processing messages.
+    let src = ipv4!([127, 0, 0, 1]);
+    let dst = ipv4!([127, 0, 0, 2]);
+    let (mut socket, _packets) = Socket::bind(src).await.expect("should bind to the address");
+    let (tx_messages, messages) = mpsc::channel(8);
+    tokio::spawn(async move { socket.process(messages).await });
+
+    // Create a native socket to receive the data.
+    let socket = UdpSocket::bind(dst)
+        .await
+        .expect("should bind to the address");
+
+    // Send the data.
+    let data = Arc::new([1u8; 16]);
+    let (message, response) = Message::new(Operation::Send((dst, data.clone())));
+    tx_messages
+        .send(message)
+        .await
+        .expect("should send the data to the socket");
+    assert!(response.await.is_ok());
+
+    // Receive the data.
+    let mut buffer = [0u8; 16];
+    let (_size, address) = socket
+        .recv_from(&mut buffer)
+        .await
+        .expect("should receive the data");
+    assert_eq!(address, src);
+    assert_eq!(buffer[..16], data[..16]);
+}
+
+/// Checks that the socket can receive data.
+///
+/// Steps:
+/// 1. Create a [`Socket`] and start processing messages.
+/// 2. Create a native socket to send the data.
+/// 3. Send the data from the native socket.
+/// 4. Receive the data on the [`Socket`].
+///
+/// Checks:
+/// - The socket should receive the data.
+/// - The socket should receive the data from the correct address.
+/// - The socket should receive the correct data.
+#[tokio::test]
+async fn recv() {
+    // Create a new [`Socket`] and start processing messages.
     let src = ipv4!([127, 0, 0, 1]);
     let dst = ipv4!([127, 0, 0, 2]);
     let (mut socket, mut packets) = Socket::bind(src).await.expect("should bind to the address");
     let (_tx_messages, messages) = mpsc::channel(8);
     tokio::spawn(async move { socket.process(messages).await });
+
+    // Create a native socket to send the data.
     let socket = UdpSocket::bind(dst)
         .await
         .expect("should bind to the address");
+
+    // Send the data.
     let buffer = [1u8; 16];
     socket
         .send_to(buffer.as_ref(), src)
         .await
         .expect("should send the data to the socket");
+
+    // Receive the data.
     let (address, data) = packets.recv().await.expect("should receive a packet");
     assert_eq!(address, dst);
     assert_eq!(data[..16], buffer[..16]);
