62ff0d76e603a333a59aa6f54c31d86b23f76ba8
brunoldsilva <brunoldsilva@pm.me>
2024-01-25 20:32:23 +0000

    Add `ResponseSender` and `ResponseReceiver` utils.
    These channels allows sending a single response from the sender
    to the receiver.

--- rsomeip/src/net/util.rs
+++ rsomeip/src/net/util.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+use tokio::sync::oneshot;
 
 #[cfg(test)]
 mod tests;
@@ -73,5 +74,91 @@ impl BufferPool {
     /// Adds the `buffer` to the pool.
     pub fn push(&mut self, buffer: Arc<[u8]>) {
         self.buffers.push(buffer);
+    }
+}
+
+/// Creates a connected pair of [`ResponseSender`] and [`ResponseReceiver`].
+///
+/// A response can be sent from the [`ResponseSender`], using either the `ok` or `err` functions,
+///  and received by the [`ResponseReceiver`] with a call to `get`.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```rust
+/// # tokio_test::block_on(async {
+/// // Create a pair for sending and receiving.
+/// let (sender, receiver) = rsomeip::net::util::response_channels::<bool, i32>();
+///
+/// // Move one of them to a different task.
+/// tokio::spawn(async move {
+///     // Send the response through the sender.
+///     sender.ok(true);
+///     // Or, sender.err(-1);
+/// });
+///
+/// // Receive the response on the receiver.
+/// let response = receiver.get().await;
+/// assert_eq!(response, Some(Ok(true)));
+/// # });
+/// ```
+pub fn response_channels<T, E>() -> (ResponseSender<T, E>, ResponseReceiver<T, E>)
+where
+    T: Send,
+    E: Send,
+{
+    let (sender, receiver) = oneshot::channel();
+    (ResponseSender::new(sender), ResponseReceiver::new(receiver))
+}
+
+/// Receives a response from the associated [`ResponseSender`].
+///
+/// A pair of [`ResponseSender`] and [`ResponseReceiver`] can be created with the
+/// [`response_channels`] function.
+pub struct ResponseReceiver<T, E> {
+    inner: oneshot::Receiver<Result<T, E>>,
+}
+
+impl<T, E> ResponseReceiver<T, E>
+where
+    T: Send,
+    E: Send,
+{
+    /// Creates a new [`ResponseReceiver`].
+    fn new(inner: oneshot::Receiver<Result<T, E>>) -> Self {
+        Self { inner }
+    }
+
+    /// Gets the response from the [`ResponseSender`].
+    ///
+    /// This function returns [`None`] if the sender is dropped before sending the response.
+    pub async fn get(self) -> Option<Result<T, E>> {
+        self.inner.await.ok()
+    }
+}
+
+/// Sends a response to the associated [`ResponseReceiver`].
+///
+/// A pair of [`ResponseSender`] and [`ResponseReceiver`] can be created with the
+/// [`response_channels`] function.
+pub struct ResponseSender<T, E> {
+    inner: oneshot::Sender<Result<T, E>>,
+}
+
+impl<T, E> ResponseSender<T, E> {
+    /// Creates a new [`ResponseSender`].
+    fn new(inner: oneshot::Sender<Result<T, E>>) -> Self {
+        Self { inner }
+    }
+
+    /// Sends an [`Ok`] response with the given value to the [`ResponseReceiver`].
+    pub fn ok(self, value: T) {
+        let _ = self.inner.send(Ok(value));
+    }
+
+    /// Sends an [`Err`] response with the given error to the [`ResponseReceiver`].
+    pub fn err(self, error: E) {
+        let _ = self.inner.send(Err(error));
     }
 }
--- rsomeip/src/net/util/tests.rs
+++ rsomeip/src/net/util/tests.rs
@@ -1,4 +1,6 @@
 use super::*;
+use std::time::Duration;
+use tokio::time::timeout;
 
 #[test]
 fn new() {
@@ -24,4 +26,45 @@ fn try_pull() {
     assert!(pool.try_pull().is_none());
     std::mem::drop(buffer);
     assert!(pool.try_pull().is_some());
+}
+
+#[tokio::test]
+async fn response_ok() {
+    let (sender, receiver) = response_channels::<(), i32>();
+    let handle = tokio::spawn(async move {
+        sender.ok(());
+    });
+    let response = timeout(Duration::from_millis(10), receiver.get())
+        .await
+        .expect("should not timeout");
+    assert_eq!(response, Some(Ok(())));
+    timeout(Duration::from_millis(10), handle)
+        .await
+        .expect("should not timeout")
+        .expect("should complete successfully");
+}
+
+#[tokio::test]
+async fn response_err() {
+    let (sender, receiver) = response_channels::<(), i32>();
+    let handle = tokio::spawn(async move {
+        sender.err(-1);
+    });
+    let response = timeout(Duration::from_millis(10), receiver.get())
+        .await
+        .expect("should not timeout");
+    assert_eq!(response, Some(Err(-1)));
+    timeout(Duration::from_millis(10), handle)
+        .await
+        .expect("should not timeout")
+        .expect("should complete successfully");
+}
+
+#[tokio::test]
+async fn response_none() {
+    let (_, receiver) = response_channels::<(), i32>();
+    let response = timeout(Duration::from_millis(10), receiver.get())
+        .await
+        .expect("should not timeout");
+    assert_eq!(response, None);
 }
